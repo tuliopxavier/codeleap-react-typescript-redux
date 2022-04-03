c92073a3ec7d641fc567fdcd6c234c3a49d074d9
tuliopxavier <tuliopxavier@gmail.com>
2022-04-03 19:51:40 -0300

    create loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,11 @@
+import { LoadingLogo } from './styled'
+import Logo from '../../public/assets/Logo.svg?component'
+
+export const Loading = () => {
+
+    return (
+        <LoadingLogo>
+            <Logo />
+        </LoadingLogo>
+    );
+};
