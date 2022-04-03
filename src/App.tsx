fa0651721a10cd13a080fb382816dbad9f8dbdfe
tuliopxavier <tuliopxavier@gmail.com>
2022-04-03 20:16:15 -0300

    add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { MainPage } from "./pages/MainPage";
 import { SignUpPage } from './pages/SignUpPage';
+import { GlobalStyle } from './globalstyles';
 
 function App() {
 
   return (
     <BrowserRouter>
+    <GlobalStyle />
       <Routes>
         <Route path="/" element={<MainPage/>} />
         <Route path="/signup" element={<SignUpPage/>} />
---
+++ src/globalStyles.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&family=Roboto:wght@400;700&display=swap');
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        background-color: #ddd;
+    }
+
+    body {
+        font-family: 'Roboto', sans-serif;
+    }
+
+    /* @media (max-width: 768px) {} */
+`
