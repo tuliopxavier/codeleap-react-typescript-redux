4fa50f2a1d9dcf89305b242d09ad34915b6b1819
tuliopxavier <tuliopxavier@gmail.com>
2022-04-03 19:52:21 -0300

    cofigure routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { MainPage } from "./pages/MainPage";
+import { SignUpPage } from './pages/SignUpPage';
+
 function App() {
 
   return (
-    <>hello world!</>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage/>} />
+        <Route path="/signup" element={<SignUpPage/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
