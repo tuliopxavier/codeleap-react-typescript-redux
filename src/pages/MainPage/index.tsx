82151f737b5d6b12f2cd32487859470097e553b3
tuliopxavier <tuliopxavier@gmail.com>
2022-04-13 03:37:29 -0300

    add test files to pages

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -11,13 +11,13 @@ import { Section, UpButton } from './styled';
 // import api from '../../actions/services/api';
 
 export const MainPage = () => {
-    const username = useSelector((state: RootState) => state.username.value);
-    const posts = useSelector((state: RootState) => state.posts.value);
+    const username = useSelector((state: RootState) => state.username?.value);
+    const posts = useSelector((state: RootState) => state.posts?.value);
     const dispatch = useDispatch();
     
     const [titleValue, setTitleValue] = useState('');
     const [contentValue, setContentValue] = useState('');
-    const [postId, setPostId] = useState(posts.length);
+    const [postId, setPostId] = useState(posts?.length);
     const [isDisabled, setIsDisabled] = useState(true);
     const [hideScrollTopButton, setHideScrollTopButton] = useState('hide-button');
 
